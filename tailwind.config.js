07424a3dc38be4d5e823a3609d70afbf14a31038
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2025-02-17 08:16:58 +0200

    add responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: "640px", // Small devices (phones)
+      md: "768px", // Medium devices (tablets)
+      lg: "1024px", // Large devices (desktops)
+      xl: "1280px", // Extra large devices (large desktops)
+      "2xl": "1536px", 
+    },
+  },
+};
